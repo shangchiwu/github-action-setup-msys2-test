32a81bcfdd1a86b04abb9da63b1a900bc930ecb3
shangchiwu <shangchi537@gmail.com>
2021-11-02 23:50:53 +0800

    modify main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,5 +4,8 @@ int main(int argc, const char **argv) {
 
     std::cout << "Hello GitHub Action!" << std::endl;
 
+    const int a = 42;
+    std::cout << "a = " << a << std::endl;
+
     return 0;
 }
