79555b6db18125136996e7801593cdc92da58498
shangchiwu <shangchi537@gmail.com>
2021-11-03 21:05:07 +0800

    add c

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,10 @@ int main(int argc, const char **argv) {
     std::cout << "a = " << a << std::endl;
 
     const int b = 87;
-    std::cout << "b = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
+
+    const int *c = nullptr;
+    std::cout << "c = " << *c << std::endl; // error here
 
     return 0;
 }
