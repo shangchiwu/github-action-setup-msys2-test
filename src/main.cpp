019f00670cffcd1af0319c68461ae82b7a300f72
shangchiwu <shangchi537@gmail.com>
2021-11-22 18:17:22 +0800

    feat: add smile :)

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc, const char **argv) {
 
-    std::cout << "Hello GitHub Action!" << std::endl;
+    std::cout << "Hello GitHub Action! :)" << std::endl;
 
     const int a = 42;
     std::cout << "a = " << a << std::endl;
